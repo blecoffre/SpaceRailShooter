5cf400507fe3cc9b321028bbb21b053047c40f04
Deathtreak <beber76.lecoffre@orange.fr>
2020-04-08 15:41:57 +0200

    Build first level , splash screen, basic player controls

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField][Tooltip("In ms^-1")] private float m_xSpeed = 20.0f;
+    [SerializeField] [Tooltip("In m")] private float m_xRange = 5.0f;
+
+    [SerializeField][Tooltip("In ms^-1")] private float m_ySpeed = 20.0f;
+    [SerializeField][Tooltip("In m")] private float m_yRange = 3.0f;
+
+    [SerializeField] private float m_positionPitchFactor = -5.0f;
+    [SerializeField] private float m_controlPitchFactor = -20.0f;
+    [SerializeField] private float m_positionYawFactor = 5.0f;
+    [SerializeField] private float m_controlRollFactor = -20.0f;
+
+    private float m_xThrow;
+    private float m_yThrow;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    private void ProcessTranslation()
+    {
+        transform.localPosition = new Vector3(XClampedPos(), YClampedPos(), transform.localPosition.z);
+    }
+
+    private float XClampedPos()
+    {
+        m_xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = m_xThrow * m_xSpeed * Time.deltaTime;
+        float rawXPos = transform.localPosition.x + xOffset;
+        return Mathf.Clamp(rawXPos, -m_xRange, m_xRange);
+    }
+
+    private float YClampedPos()
+    {
+        m_yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = m_yThrow * m_ySpeed * Time.deltaTime;
+        float rawYPos = transform.localPosition.y + yOffset;
+        return Mathf.Clamp(rawYPos, -m_yRange, m_yRange);
+    }
+
+    private void ProcessRotation()
+    {
+        transform.localRotation = Quaternion.Euler(Pitch(), Yaw(), Roll());
+    }
+
+    private float Roll()
+    {
+        return m_xThrow * m_controlRollFactor;
+    }
+
+    private float Yaw()
+    {
+        return transform.localPosition.x * m_positionYawFactor;
+    }
+
+    private float Pitch()
+    {
+        float pitchDueToPosition = transform.localPosition.y * m_positionPitchFactor;
+        float pitchDueToControlThrow = m_yThrow * m_controlPitchFactor;
+
+        return pitchDueToPosition * pitchDueToControlThrow * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/SplashScreenLoader.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SplashScreenLoader : MonoBehaviour
+{
+    [SerializeField]
+    private Image m_loadingBar;
+    [SerializeField]
+    private float m_timeBeforeLoadingMenu = 5.0f;
+
+    private AsyncOperation asyncLoadScene;
+
+    private void Start()
+    {
+        StartCoroutine(LoadAsynchronously("Level1"));
+
+        Invoke("ActiveMenu", m_timeBeforeLoadingMenu); //minial time in splash screen
+    }
+
+    IEnumerator LoadAsynchronously(string levelName)
+    {
+        float timer = 0f;
+
+        AsyncOperation operation = SceneManager.LoadSceneAsync(levelName);
+        operation.allowSceneActivation = false;
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            m_loadingBar.fillAmount = progress;
+            timer += Time.deltaTime;
+
+            if (timer > m_timeBeforeLoadingMenu)
+            {
+                operation.allowSceneActivation = true;
+            }
+            yield return null;
+        }
+        yield return null;
+    }
+}
